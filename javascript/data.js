3d859fcad0dc807d70514bddc1f45d96f8e0cf6f
elTorresPro <desportador@gmail.com>
2023-04-24 19:27:23 -0500

    javascript

---
+++ javascript/data.js
@@ -0,0 +1,29 @@
+//PRODUCTOS
+class Producto {
+    constructor(id,tipo,prod, descripcion, precio,img,cantidad) {
+        this.id=id
+        this.tipo=tipo
+        this.product = prod
+        this.descripcion = descripcion
+        this.precio = precio   
+        this.img= img
+        this.cantidad= cantidad
+    }
+}
+
+const producto1 = new Producto('001','diseño', 'Tarjetas de Presentación','Servicio de Creación de editable de la tarjeta de presentación.', 50,'https://picsum.photos/id/20/600','20')
+const producto2 = new Producto('002','diseño','Fotografía','Servicio de Sesión de  fotos para eventos o empresarial.', 100,'https://picsum.photos/id/435/600','50')
+const producto3= new Producto('003','impresion','Impresión en Vinil', 'Impresión de papeleria empresarial y publicitaria.', 80,'https://picsum.photos/id/274/600','30')
+const producto4 = new Producto('004','diseño','Almanaques', 'Servicio de Creación de Almanaques.', 50,'https://picsum.photos/id/668/600','20')
+const producto5 = new Producto('005','impresion','Merchandising','Realizado de Merchandising para presentación Empresarial', 120,'https://picsum.photos/id/370/600','10')
+const producto6 = new Producto('006','instalacion','Panel Publicitario','Instalación de Panel Publicitario y construccion de Panel', 500,'https://picsum.photos/id/405/600','15')
+const producto7 = new Producto('007','diseño','Señaletica', 'Diseño e instalacion de señaletica.', 70,'https://picsum.photos/id/619/600','12')
+const producto8 = new Producto('008','diseño','Portafolios',  'Diseño de Portafolios empresariales.', 40,'https://picsum.photos/id/526/600','80')
+const producto9 = new Producto('009','diseño','Páginas Web', 'Diseño de Paginas Web.', 1200,'https://picsum.photos/id/1/600','50')
+const producto10 = new Producto('010','diseño','Logos',  'Diseño y creacion de logos.', 150,'https://picsum.photos/id/180/600','40')
+const producto11 = new Producto('011','instalacion','Letrero Luminoso', 'Instalacion de Letreros Luminosos.', 700,'https://picsum.photos/id/1033/600','20')
+const producto12 = new Producto('012','instalacion','Valla Publicitaria',  'Instalacion de Valla Publicitaria.', 750,'https://picsum.photos/id/451/600','12')
+const producto13 = new Producto('013','instalacion','Instalación en Vinil', 'Intalacion y tralado de vinil para fachada de negocios.', 400,'https://picsum.photos/id/442/600','12')
+const producto14 = new Producto('014','diseño','Comunity Manager',  'Dedicacion y tiempo para el seguimiento de las paginas.', 300,'https://picsum.photos/id/445/600','40')
+const producto15 = new Producto('015','impresion','Papelería Empresarial',  'Impresion de material para papeleria empresarial.', 80,'https://picsum.photos/id/163/600','10')
+
---
+++ javascript/index.js
@@ -0,0 +1,147 @@
+//ARRAY PRODUCTOS
+let productos = []
+
+
+//ARRAY CARRITO
+let carrito= []
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carritoContenedor')
+const botonVaciar = document.getElementById('vaciarCarrito')
+const botonConfirmarCompra = document.querySelector('#confirmarCompra')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
+const productosContainer = document.querySelector('#productosContainer')
+const searchBar = document.querySelector('#searchBar')
+const searchButton = document.querySelector('#searchButton')
+
+//GET LOCAL STORAGE
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+//PRODUCTOS
+const renderizarProductos= () =>{
+   productos.forEach ((producto) => {
+    const nuevoDiv = document.createElement('div')
+    nuevoDiv.innerHTML = `
+            <img src="${producto.img}" class="box__img" alt="producto">
+            <h1 class="box__title"> ${producto.product} </h1>
+            <p class="box__text" >${producto.descripcion}</p>
+            <h2 class="Box_price">$ ${producto.precio}</h2>
+            <button id="agregar${producto.id}" class="button">Añadir al Carrito <i class="fas fa-shopping-cart"></i></button>
+    `
+    nuevoDiv.className = 'box'
+    productosContainer.append(nuevoDiv)
+    const boton = document.getElementById(`agregar${producto.id}`)
+    boton.addEventListener('click', () => {        
+    agregarAlCarrito(producto.id)        
+    })
+    boton.addEventListener('click',()=>{
+        Swal.fire({
+            position: 'bottom-center',
+            icon: 'success',
+            color:'white',
+            background: '#535252',
+            title: 'Se ha agregado al carrito',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    })
+})
+
+//AGREGAR AL CARRITO
+const agregarAlCarrito = (prodId) => {
+    const AgregarCantidad = carrito.some (prod => prod.id === prodId) 
+    if (AgregarCantidad){ 
+        const prod = carrito.map (prod => { 
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+        actualizarCarrito()
+    } else {
+    const item = productos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    }}
+
+}
+//ELIMINAR DEL CARRITO
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item) 
+    carrito.splice(indice, 1) 
+    actualizarCarrito()     
+}
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <img src="${prod.img}" class="carritoImg" alt="producto">
+        <p>${prod.product}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="botonEliminar"><i class="fas fa-trash"></i></button>
+        `
+        contenedorCarrito.appendChild(div)      
+    })
+
+//SET LOCAL STORAGE
+localStorage.setItem('carrito', JSON.stringify(carrito))
+//CONTADOR DEL CARRITO
+contadorCarrito.innerText = carrito.length 
+//PRECIO TOTAL CARRITO
+precioTotal.innerText = carrito.reduce((acumulador, prod) => acumulador + prod.cantidad * prod.precio, 0)    
+}
+
+//VACIAR CARRITO
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+//CONFIRMAR COMPRA
+botonConfirmarCompra.addEventListener('click', () => {
+    if (carrito.length === 0){
+     Swal.fire({
+            position: 'bottom-center',
+            icon: 'error',
+            color:'white',
+            background: '#535252',
+            title: 'No hay productos en el carrito',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }     else   Swal.fire({
+        title: '¡Gracias por tu Compra!',
+        text: 'Gracias por su compra, su total es de $ ' + precioTotal.innerText,
+        color:'white',
+        background: '#535252',
+        confirmButtonColor: 'black',
+        showClass: {
+          popup: 'animate__animated animate__backInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__backOutUp'
+        }
+         }) 
+    
+    })
+   
+const getproductos = async()=> {
+    const response = await fetch ('../json/servicios.json')
+    const data=await response.json()
+    productos = data
+    renderizarProductos (productos)    
+}
+ getproductos()
+
